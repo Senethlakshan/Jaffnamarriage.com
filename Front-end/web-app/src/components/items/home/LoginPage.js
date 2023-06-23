65366b2064d3233df01607abb72155f674690030
Senethlakshan <81217773+Senethlakshan@users.noreply.github.com>
2023-06-23 15:54:19 +0530

    update login,register pages

---
+++ Front-end/web-app/src/components/items/home/LoginPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const LoginPage = () => {
+  
+
+  return (
+   <div>
+    <div>
+      
+       <h1>login</h1>
+    </div>
+   </div>
+      
+  );
+};
+
+export default LoginPage;
---
+++ Front-end/web-app/src/components/items/home/RegisterPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const RegisterPage = () => {
+  
+
+  return (
+   <div>
+    <div>
+      
+       <h1>Register</h1>
+    </div>
+   </div>
+      
+  );
+};
+
+export default RegisterPage;
