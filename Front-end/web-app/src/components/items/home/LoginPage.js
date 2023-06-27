96b05c8424d21808f4f05441453ff49d517a75b4
Senethlakshan <81217773+Senethlakshan@users.noreply.github.com>
2023-06-27 14:10:31 +0530

    update

--- Front-end/web-app/src/components/items/home/LoginPage.js
+++ Front-end/web-app/src/components/items/home/LoginPage.js
@@ -6,13 +6,10 @@ const LoginPage = () => {
 
   return (
    <div>
-<<<<<<< Updated upstream
     <div>  
        <h1>login</h1>
     </div>
-=======
    <h1>LoginPage</h1>
->>>>>>> Stashed changes
    </div>
       
   );
