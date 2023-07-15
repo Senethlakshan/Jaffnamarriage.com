255d27a74f11691d957ee858ac229a5e67207764
Senethlakshan <81217773+Senethlakshan@users.noreply.github.com>
2023-07-15 14:47:40 +0530

    update

--- Front-end/web-app/src/components/items/home/LoginPage.js
+++ Front-end/web-app/src/components/items/home/LoginPage.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
-import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { FaEye, FaEyeSlash,FaUserAlt } from 'react-icons/fa';
 import { useSpring, animated } from 'react-spring';
 import LottieAnimation from 'lottie-react';
-import animationData from '../../../assests/home/bg-remover/lottie/login.json'; 
+import animationData from '../../../assests/home/bg-remover/lottie/login.json';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function LoginPage() {
   const fadeAnimation = useSpring({
@@ -12,82 +15,137 @@ function LoginPage() {
   });
 
   const [passwordVisible, setPasswordVisible] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [rememberMe, setRememberMe] = useState(false);
 
   const togglePasswordVisibility = () => {
     setPasswordVisible(!passwordVisible);
   };
 
-  return (
-    <div className="min-h-screen">
-      
-      <animated.div className=" " style={fadeAnimation}>
-      
-      <div className="flex h-screen ">
-      <div className="w-1/2 bg-gradient-to-t from-amber-900 to-yellow-300 flex items-center justify-center">
-      <LottieAnimation
-        lottieRef={(ref) => ref && ref.setSpeed(0.5)}
-        animationData={animationData}
-        loop={true}
-        autoplay={true}
-      />
-      </div>
-      <div className="w-1/2 bg-white flex items-center justify-center shadow-lg">
-      <div className="p-6 max-w-sm w-full">
-        <h2 className="text-2xl font-semibold text-gray-700 mb-6">Login</h2>
-        <form>
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-gray-700 text-sm font-bold mb-2"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              placeholder="Enter your email"
-              className="w-full px-4 py-3 rounded-lg bg-gray-100 border-gray-300 focus:border-blue-500 focus:bg-white focus:outline-none mb-4"
-            />
-          </div>
-          <div className="relative">
-            <label
-              htmlFor="password"
-              className="block text-gray-700 text-sm font-bold mb-2"
-            >
-              Password
-            </label>
-            <input
-              type={passwordVisible ? 'text' : 'password'}
-              id="password"
-              placeholder="Enter your password"
-              className="w-full px-4 py-3 rounded-lg bg-gray-100 border-gray-300 focus:border-blue-500 focus:bg-white focus:outline-none pr-10 mb-4"
-            />
-            <button
-              type="button"
-              onClick={togglePasswordVisibility}
-              className="absolute inset-y-0 right-0 flex items-center pr-3"
-            >
-              {passwordVisible ? (
-                <FaEyeSlash className="h-5 w-5 text-gray-500" />
-              ) : (
-                <FaEye className="h-5 w-5 text-gray-500" />
-              )}
-            </button>
-          </div>
-        </form>
-        <button
-          type="button"
-          className="w-full py-3 rounded-lg bg-blue-500 text-white font-bold hover:bg-blue-700"
-        >
-          Login
-        </button>
-      </div>
-    </div>
-    </div>
+  const handleLogin = (e) => {
+    e.preventDefault();
 
+    // Perform email and password validation
+    if (!validateEmail(email)) {
+      toast.error('Invalid email format');
+      return;
+    }
+
+    if (password.length < 6) {
+      toast.error('Password must be at least 6 characters long');
+      return;
+    }
+
+    // Perform login logic
+    // ...
+
+    // Display success message
+    toast.success('Login successful');
+  };
+
+  const validateEmail = (email) => {
+    // Use a regular expression to validate email format
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  };
+
+  return (
+    <div className="min-h-screen bg-gradient-to-t from-amber-900 to-yellow-300">
+      <animated.div className="" style={fadeAnimation}>
+        {/* animated svg */}
+        <div className="flex h-screen">
+          <div className="w-1/2 flex items-center justify-center">
+            <LottieAnimation
+              lottieRef={(ref) => ref && ref.setSpeed(0.5)}
+              animationData={animationData}
+              loop={true}
+              autoplay={true}
+            />
+          </div>
+          {/* login from */}
+          <div className="w-1/2 flex items-center justify-center  " >
+          <div className="p-6 max-w-sm w-full border-2 border-solid border-gray-300 rounded-lg bg-white">
+            <h2 className="text-2xl font-semibold  flex items-center mb-6">
+                <FaUserAlt className="mr-2" />
+                Login
+              </h2>
+              <form onSubmit={handleLogin}>
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    placeholder="Enter your email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="w-full px-4 py-3 rounded-lg bg-gray-100 border-gray-300 focus:border-blue-500 focus:bg-white focus:outline-none mb-4"
+                  />
+                </div>
+                <div className="relative">
+                  <label
+                    htmlFor="password"
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type={passwordVisible ? 'text' : 'password'}
+                    id="password"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="w-full px-4 py-3 rounded-lg bg-gray-100 border-gray-300 focus:border-blue-500 focus:bg-white focus:outline-none pr-10 mb-4"
+                  />
+                  <button
+                    type="button"
+                    onClick={togglePasswordVisibility}
+                    className="absolute inset-y-0 right-0 flex items-center pr-3"
+                  >
+                    {passwordVisible ? (
+                      <FaEyeSlash className="h-5 w-5 text-gray-500" />
+                    ) : (
+                      <FaEye className="h-5 w-5 text-gray-500" />
+                    )}
+                  </button>
+                </div>
+                <div className="flex items-center justify-between mb-4">
+                  <label className="flex items-center">
+                    <input
+                      type="checkbox"
+                      className="form-checkbox h-4 w-4 text-blue-500"
+                      checked={rememberMe}
+                      onChange={() => setRememberMe(!rememberMe)}
+                    />
+                    <span className="ml-2 text-gray-700">Remember me</span>
+                  </label>
+                  <a
+                    href="#"
+                    className="text-sm text-blue-500 hover:text-blue-700"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+                <button
+                  type="submit"
+                  className="w-full py-3 rounded-lg bg-gradient-to-tr from-amber-900 to-yellow-300 text-white font-bold "
+                >
+                  Login
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <ToastContainer />
       </animated.div>
     </div>
   );
 }
 
 export default LoginPage;
+
--- Front-end/web-app/src/components/items/home/RegisterPage.js
+++ Front-end/web-app/src/components/items/home/RegisterPage.js
@@ -1,17 +1,223 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaEye, FaEyeSlash,FaUserAlt } from 'react-icons/fa';
+import { useSpring, animated } from 'react-spring';
 import LottieAnimation from 'lottie-react';
-import animationData from '../../../assests/home/bg-remover/lottie/Astronot.json'; // Replace with the path to your downloaded Lottie animation file
+import animationData from '../../../assests/home/bg-remover/l2/goldenhart.json';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function RegisterPage() {
+  const fadeAnimation = useSpring({
+    from: { opacity: 0, transform: 'translateY(20px)' },
+    to: { opacity: 1, transform: 'translateY(0)' },
+    config: { duration: 500 },
+  });
+
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [name, setName] = useState('');
+  const [marriedStatus, setMarriedStatus] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [isChecked, setIsChecked] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+
+    // Check if terms and conditions checkbox is checked
+    if (!isChecked) {
+      toast.error('Please accept the terms and conditions');
+      return;
+    }
+
+    // Perform field validations
+    if (!validateName(name)) {
+      toast.error('Invalid name');
+      return;
+    }
+
+    if (!validateMarriedStatus(marriedStatus)) {
+      toast.error('Invalid married status');
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      toast.error('Invalid email format');
+      return;
+    }
+
+    if (password.length < 6) {
+      toast.error('Password must be at least 6 characters long');
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      toast.error('Passwords do not match');
+      return;
+    }
+
+    // Perform registration logic
+    // ...
+
+    // Display success message
+    toast.success('Registration successful');
+  };
+
+  const validateName = (name) => {
+    return name.trim().length > 0;
+  };
+
+  const validateMarriedStatus = (status) => {
+    return ['single', 'married', 'divorce'].includes(status);
+  };
+
+  const validateEmail = (email) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  };
+
   return (
-    <div>
-      <h1>login page</h1>
-      <LottieAnimation
-        lottieRef={(ref) => ref && ref.setSpeed(0.5)} // Optional: Adjust animation speed
-        animationData={animationData}
-        loop={true}
-        autoplay={true}
-      />
+    <div className="min-h-screen bg-gradient-to-t from-amber-900 to-yellow-300">
+      <animated.div className="" style={fadeAnimation}>
+        {/* animated svg */}
+        <div className="flex h-screen">
+          <div className="w-1/2 flex items-center justify-center">
+            <LottieAnimation
+              lottieRef={(ref) => ref && ref.setSpeed(0.5)}
+              animationData={animationData}
+              loop={true}
+              autoplay={true}
+            />
+         <div>
+         <h1 className=' text-3xl text-white' 
+          style={{
+            fontFamily: 'Berkshire Swash, cursive'
+          }}
+         >
+          "Marriage is a new beginning, let it be completed with just one eye."</h1>
+          <p className='mt-2 text-gray-100 '>திருமணம் ஒரு புதிய ஆரம்பம், அதை ஒரு கண்ணே முடிக்க வேண்டும்.</p>
+          </div>   
+            
+          </div>
+          {/* login from */}
+          <div className="w-1/2 flex items-center justify-center  ">
+            <div className="w-full max-w-sm p-6 bg-white rounded-lg shadow-lg">
+              <h2 className="text-2xl font-semibold flex items-center mb-6">
+                <FaUserAlt className="mr-2" />
+                Register
+              </h2>
+              <form onSubmit={handleRegister}>
+                <div className="mb-4">
+                  <label htmlFor="name" className="block text-gray-700 text-sm font-bold mb-2">
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    placeholder="Enter your name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="w-full px-4 py-3 rounded-lg bg-gray-100 border-gray-300 focus:border-blue-500 focus:bg-white focus:outline-none"
+                  />
+                </div>
+                <div className="mb-4">
+                  <label htmlFor="marriedStatus" className="block text-gray-700 text-sm font-bold mb-2">
+                    Married Status
+                  </label>
+                  <select
+                    id="marriedStatus"
+                    value={marriedStatus}
+                    onChange={(e) => setMarriedStatus(e.target.value)}
+                    className="w-full px-4 py-3 rounded-lg bg-gray-100 border-gray-300 focus:border-blue-500 focus:bg-white focus:outline-none"
+                  >
+                    <option value="">Select Status</option>
+                    <option value="single">Single</option>
+                    <option value="married">Married</option>
+                    <option value="divorce">Divorce</option>
+                  </select>
+                </div>
+                <div className="mb-4">
+                  <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    placeholder="Enter your email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="w-full px-4 py-3 rounded-lg bg-gray-100 border-gray-300 focus:border-blue-500 focus:bg-white focus:outline-none"
+                  />
+                </div>
+                <div className="mb-4 relative">
+                  <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">
+                    Password
+                  </label>
+                  <div className="relative">
+                    <input
+                      type={passwordVisible ? 'text' : 'password'}
+                      id="password"
+                      placeholder="Enter your password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      className="w-full px-4 py-3 rounded-lg bg-gray-100 border-gray-300 focus:border-blue-500 focus:bg-white focus:outline-none pr-10"
+                    />
+                    <button
+                      type="button"
+                      onClick={togglePasswordVisibility}
+                      className="absolute inset-y-0 right-0 flex items-center pr-3"
+                    >
+                      {passwordVisible ? (
+                        <FaEyeSlash className="h-5 w-5 text-gray-500" />
+                      ) : (
+                        <FaEye className="h-5 w-5 text-gray-500" />
+                      )}
+                    </button>
+                  </div>
+                </div>
+                <div className="mb-4">
+                  <label htmlFor="confirmPassword" className="block text-gray-700 text-sm font-bold mb-2">
+                    Confirm Password
+                  </label>
+                  <input
+                    type="password"
+                    id="confirmPassword"
+                    placeholder="Confirm your password"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    className="w-full px-4 py-3 rounded-lg bg-gray-100 border-gray-300 focus:border-blue-500 focus:bg-white focus:outline-none"
+                  />
+                </div>
+                <div className="mb-4">
+                  <label className="flex items-center">
+                    <input
+                      type="checkbox"
+                      className="form-checkbox h-4 w-4 text-blue-500"
+                      checked={isChecked}
+                      onChange={() => setIsChecked(!isChecked)}
+                    />
+                    <span className="ml-2 text-gray-700">
+                      I agree to the <a href="#">Terms and Conditions</a>
+                    </span>
+                  </label>
+                </div>
+                <button
+                  type="submit"
+                  className="w-full py-3 rounded-lg bg-gradient-to-tr from-amber-900 to-yellow-300 text-white font-bold"
+                  disabled={!isChecked}
+                >
+                  Register
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </animated.div>
+      <ToastContainer />
     </div>
   );
 }
